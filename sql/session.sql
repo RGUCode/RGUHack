a78e7591f14ff6e9dc2531366a370ba71beb47bb
Luke Fisher <mail@lukefisher.me>
2017-04-04 15:22:47 +0100

    Update SQL files

---
+++ sql/session.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `user` (
+  `session` char(128) NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- sql/user.sql
+++ sql/user.sql
@@ -1,8 +1,12 @@
-CREATE TABLE `student` (
+CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) DEFAULT NULL,
-  `place_study` varchar(200) DEFAULT NULL,
   `email` varchar(256) NOT NULL,
+  `password` char(60) NOT NULL,
+  `place_study` varchar(200) DEFAULT NULL,
+  `dietary` varchar(500) DEFAULT NULL,
+  `shirt_size` varchar(25) DEFAULT NULL,
+  `date_birth` date NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

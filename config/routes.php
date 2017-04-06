62b5e62a386a742cc022aa3caf0a7e68a9141aba
Luke Fisher <mail@lukefisher.me>
2017-04-06 12:44:15 +0100

    Add names for some routes

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
 // Routes
 
 $app->get('/', 'Site\Controller\MainController:index');
-$app->post('/sponsor', 'Site\Controller\MainController:sponsor');
-$app->post('/register', 'Site\Controller\MainController:register');
+$app->post('/sponsor', 'Site\Controller\MainController:sponsor')->setName('sponsor');
+$app->post('/register', 'Site\Controller\MainController:register')->setName('student_register');
 
 $app->map(['GET', 'POST'], '/confirm/{token:[0-9a-f]+}', 'Site\Controller\MainController:confirm')->setName('confirm');
